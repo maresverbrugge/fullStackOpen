e8406303899a15e053948a105084e738f7477283
Mares Verbrugge <maresverbrugge@mbp-van-mares.home>
2024-09-19 13:31:14 +0200

    start of part1 exercises

---
+++ part1/courseInfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+      <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+      <p>
+        <strong>Part {props.part.partCount}: </strong>
+        "{props.part.name}"
+        Exercises: {props.part.exercises}
+      </p>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+      <div>
+       <Part part={props.parts[0]} />
+       <Part part={props.parts[1]} />
+       <Part part={props.parts[2]} />
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+      <p>Number of exercises {props.parts[0].exercises  + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course =
+  {
+    name: 'Half Stack application development',
+    parts: [
+    { partCount: 1, name: 'Fundamentals of React', exercises: 10},
+    { partCount: 2, name: 'Using props to pass data', exercises: 7},
+    { partCount: 3, name: 'State of a component', exercises: 14},
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts ={course.parts} />
+      </div>
+  )
+}
+
+export default App
