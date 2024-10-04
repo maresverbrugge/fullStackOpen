ccf969dc419d8d5e2770fc20d105a381a286f52c
Mares Verbrugge <mverbrug@f1r1s11.codam.nl>
2024-10-04 15:39:52 +0200

    progress

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,29 +1,31 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => <button onClick={onClick}> {text} </button>
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-  setCounter(counter + 1)}
-  const decreaseByOne = () => setCounter(counter - 1)
-  const increaseByFive = () => setCounter(counter + 5)
-  const decreaseByFive = () => setCounter(counter - 5)
-  const setToZero = () => setCounter(0)
-
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='+ 1'/>
-      <Button onClick={decreaseByOne} text='- 1'/>
-      <Button onClick={increaseByFive} text='+ 5'/>
-      <Button onClick={decreaseByFive} text='- 5'/>
-      <Button onClick={setToZero} text='set to 0'/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
